11109dd6c8aecdcd027b1df2c0e3f6e85f9e4bff
benjaminvickers <benjamin.vickers@yahoo.co.uk>
2016-01-10 22:33:58 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# node-express-mail
+A basic and lean recipe for implementing sockets in a node express server.
+
+To install
+-
+npm install
+
+To run
+-
+sudo npm start
+
+To test in a browser
+-
+http://localhost/
---
+++ app.js
@@ -0,0 +1,29 @@
+'use strict'
+
+var express = require('express');
+var io = require('socket.io')();
+
+var app = express();
+app.io = io;
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+var routes = require('./routes/index');
+app.use('/', routes);
+
+io.on('connection', function(socket) {
+    console.log( "A user connected" );
+    
+    socket.on('disconnect', function(){
+      console.log('A user disconnected');
+    });
+    
+    socket.on('browserEvent', function(eventMessage){
+    
+      console.log(eventMessage);
+      io.emit('serverEvent', 'Emitting event from server');
+    });
+});
+
+module.exports = app;
